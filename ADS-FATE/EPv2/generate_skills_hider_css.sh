bc9f329a6f9622895cb27c919d27ebd21074ffd3
Andre Vaillancourt <vaillancourta@ainfosec.com>
2020-08-30 17:49:44 -0400

    EPv2 WIP.

--- ADS-FATE/EPv2/generate_skills_hider_css.sh
+++ ADS-FATE/EPv2/generate_skills_hider_css.sh
@@ -10,9 +10,9 @@ i=0
 while read line; do
     IFS=':' read -ra line_ary <<< "${line}"
     if [[ ! -z "${line}" ]]; then
-        line_nospaces=$(echo "${line_ary[0]}" | sed "s/\s//g")
+        # line_nospaces=$(echo "${line_ary[0]}" | sed "s/\s//g")
         line_underscore=$(echo "${line_ary[0],,}" | sed "s/\s/_/g")
-        echo '.charsheet .sheet-ep2-skill-hider-'"${line_underscore}"':checked ~ .sheet-ep2-skill-hider-section-'"${line_underscore}"',' >> skills_hider.css
+        echo '.charsheet .sheet-EPv2skill-hider-'"${line_underscore}"':checked ~ .sheet-EPv2skill-hider-section-'"${line_underscore}"',' >> skills_hider.css
     fi
     ((i+=1))
 done < ./skills_list_alphabetical.txt
